8e8adb18e419e98bce862159b79a7510c8b69bd0
brcOne <amv.0280@gmail.com>
2025-02-07 01:24:26 +0100

    first commit

--- App/resources/js/router.js
+++ App/resources/js/router.js
@@ -2,16 +2,27 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./Pages/Home.vue"; // Le layout principal
 import Products from "./Pages/Product.vue";
 
+
 const routes = [
     {
         path: "/",
-        component: Home, // Layout principal
-        redirect: "/products", // Redirection vers Produits par défaut
+        component: Home, 
+        redirect: "/products",
         children: [
             { path: "products", component: Products },
 
-        ],
+        ]
     },
+    {
+        path : "/user/profile", name: "user.profile", component : ()=> import("./Pages/user/userProfile.vue")
+    },
+    
+    {
+        path : "/regiser", name: "register", component : ()=> import("./Pages/auth/Register.vue")
+    },
+    {
+        path : "/login", name: "login", component : ()=> import("./Pages/auth/Login.vue")
+    }
 ];
 
 const router = createRouter({
