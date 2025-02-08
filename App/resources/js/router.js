e47b3c7e44ffac24de6550715854a57fe7ff3137
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2025-02-08 12:43:45 +0100

    fixing router.js

--- App/resources/js/router.js
+++ App/resources/js/router.js
@@ -3,32 +3,28 @@ import Home from "./Pages/admin/Home.vue"; // Le layout principal
 import Products from "./Pages/admin/Product.vue";
 import Users from "./Pages/admin/Users.vue"; // Import the Users component
 
-
 const routes = [
     {
         path: "/",
-        component: Home, 
+        component: Home,
         redirect: "/products",
-        children: [
-            { path: "products", component: Products },
-
-
-        ]
+        children: [{ path: "products", component: Products }],
     },
     {
-        path : "/user/profile", name: "user.profile", component : ()=> import("./Pages/user/userProfile.vue")
-
-            { path: "users", component: Users },
-        ],
-
-    },
-    
-    {
-        path : "/regiser", name: "register", component : ()=> import("./Pages/auth/Register.vue")
+        path: "/user/profile",
+        name: "user.profile",
+        component: () => import("./Pages/user/userProfile.vue"),
     },
     {
-        path : "/login", name: "login", component : ()=> import("./Pages/auth/Login.vue")
-    }
+        path: "/regiser",
+        name: "register",
+        component: () => import("./Pages/auth/Register.vue"),
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: () => import("./Pages/auth/Login.vue"),
+    },
 ];
 
 const router = createRouter({
