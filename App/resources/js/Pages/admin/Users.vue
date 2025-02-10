e30a6b4687ff5275c0b9ca22e1d9730f03cd0839
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-02-10 12:41:18 +0100

    Users view is done

--- App/resources/js/Pages/admin/Users.vue
+++ App/resources/js/Pages/admin/Users.vue
@@ -84,6 +84,7 @@ const deleteConfirmed = async () => {
 
 
 <template>
+    <!-- Affichage de la liste des utilisateurs -->
     <div class="flex min-h-screen w-full flex-col bg-muted/40">
         <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
             <main class="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
